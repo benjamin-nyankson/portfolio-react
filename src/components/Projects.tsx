eedac2bb3dfa578a6b2b0ea8b68ff540fe845c4c
Benjamin Nyankson <benjamin.nyankson@amalitech.com>
2025-06-27 16:08:11 +0000

    feat: add ai url

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,40 +1,54 @@
 import ProjectCard from "./ProjectCard";
+import img from "../assets/favicon.png"
 
 const listOfProjects = [
   {
+    name: "Smart AI",
+    type: "React",
+    img_url:
+      img,
+    link: "https://chat-wizard-nine.vercel.app/",
+  },
+  {
     name: "Project Time Line",
     type: "Vue",
-    img_url: "https://portfolio-website-rho-lovat.vercel.app/assets/timeline-SIOEYKQm.png",
+    img_url:
+      "https://portfolio-website-rho-lovat.vercel.app/assets/timeline-SIOEYKQm.png",
     link: "https://time-line-livid.vercel.app/",
   },
   {
     name: "E-shop",
     type: "Vue",
-    img_url: "https://portfolio-website-rho-lovat.vercel.app/assets/e-com-a1BNCC49.png",
+    img_url:
+      "https://portfolio-website-rho-lovat.vercel.app/assets/e-com-a1BNCC49.png",
     link: "https://e-com-three-tan.vercel.app/",
   },
   {
     name: "E-learning",
     type: "Vue",
-    img_url: "https://portfolio-website-rho-lovat.vercel.app/assets/e-lerning-0AzyyjdS.png",
+    img_url:
+      "https://portfolio-website-rho-lovat.vercel.app/assets/e-lerning-0AzyyjdS.png",
     link: "https://e-learning-dashboard-31k8xgwp6-nyankson-benjamin.vercel.app",
   },
   {
     name: "Mini E-commerce",
     type: "Vue",
-    img_url: "https://portfolio-website-rho-lovat.vercel.app/assets/e-commerce-6GwpTu5C.png",
+    img_url:
+      "https://portfolio-website-rho-lovat.vercel.app/assets/e-commerce-6GwpTu5C.png",
     link: "https://e-commerce-app-vue.vercel.app/",
   },
   {
     name: "Youtube Clone",
     type: "Vue",
-    img_url: "https://portfolio-website-rho-lovat.vercel.app/assets/youtube-clone-TPg68Rcs.png",
+    img_url:
+      "https://portfolio-website-rho-lovat.vercel.app/assets/youtube-clone-TPg68Rcs.png",
     link: "https://youtube-three-delta.vercel.app",
   },
   {
     name: "Travelling App",
     type: "Vue",
-    img_url: "https://portfolio-website-rho-lovat.vercel.app/assets/react-png-AnaWmz9K.png",
+    img_url:
+      "https://portfolio-website-rho-lovat.vercel.app/assets/react-png-AnaWmz9K.png",
     link: "https://travelling-app-rho.vercel.app/",
   },
 ];
