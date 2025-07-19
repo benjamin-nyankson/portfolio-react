3fb712efe210309935d869c37c0618faa7890042
Benjamin Nyankson <miles@gmail.com>
2025-07-19 16:46:18 +0000

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 const Footer = () => {
+  const year = new Date().getFullYear()
     return (
       <footer className="bg-gray-800 text-white py-8 text-center">
-        <p>&copy; 2024 Benjamin Nyankson. All rights reserved.</p>
+        <p>&copy; {year} Benjamin Nyankson. All rights reserved.</p>
       </footer>
     );
   };
